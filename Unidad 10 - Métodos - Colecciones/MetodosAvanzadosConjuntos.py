1e817151e948cbf18db788c0dbdb6a23977535a4
Ariel Ayala <arielayalamail@gmail.com>
2020-01-05 21:15:32 -0300

    Métodos avanzados de conjuntos - parte 2

--- Unidad 10 - Métodos - Colecciones/MetodosAvanzadosConjuntos.py
+++ Unidad 10 - Métodos - Colecciones/MetodosAvanzadosConjuntos.py
@@ -15,3 +15,22 @@ c1 = {1, 2, 3}
 c2 = {3, 4, 5}
 
 print(c1.difference(c2))
+
+# El método difference_update() Guarda en el conjunto los elementos no comunes entre dos conjuntos
+print(c1.difference_update(c2))
+
+# El método intersection() guarda un conjunto con los elemenos comunes de dos conjuntos
+c1 = {1, 2, 3}
+c2 = {3, 4, 5}
+
+print("Los elementos comunes son :", c1.intersection(c2))
+
+# El método intersection_update() guarda en el conjunto los valores comunes de dos conjuntos
+c1.intersection_update(c2)
+print(c1)
+
+# El método symmetric_difference() devuelve los elementos simétricamente diferentes entre dos conjuntos
+c1 = {1, 2, 3}
+c2 = {3, 4, 5}
+print("Los valores diferentes entre los conjuntos son :",
+      c1.symmetric_difference(c2))
