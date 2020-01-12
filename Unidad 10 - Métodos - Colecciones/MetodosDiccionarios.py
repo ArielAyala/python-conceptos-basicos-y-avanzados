9ada05b34bf72147f6983bac5e232921f860be00
Ariel Ayala <arielayalamail@gmail.com>
2020-01-12 20:13:42 -0300

    Métodos de diccionarios - Parte 2

--- Unidad 10 - Métodos - Colecciones/MetodosDiccionarios.py
+++ Unidad 10 - Métodos - Colecciones/MetodosDiccionarios.py
@@ -5,3 +5,20 @@ print(colores.get('azul', 'No se encuentra'))
 
 # El método keys() genera y retorna una lista de los llaves del diccionario
 print(" La lista de keys es :", colores.keys())
+
+# El método value() genera una lista de los valores
+print("La lista de valores es :", colores.values())
+
+# El método items() muestra la clave y el valor
+print("Mostranto clave y valor con items() :", colores.items())
+
+for clave, valor in colores.items():
+    print(clave, valor)
+
+# El método pop() extrae un registro a partir de su key , acepta valor por defecto
+print("El valor...usando pop es :", colores.pop("amarillo", "No se encuentra"))
+
+# El método clear() borrar todos los valores
+colores.clear()
+
+print("Mostrando diccionario vacío :", colores)
